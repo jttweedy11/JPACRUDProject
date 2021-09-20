5e8c675ea11ed966c0cf2eeaee400f210c861880
Jacob Tweedy <jttweedy11@gmail.com>
2021-09-20 01:16:13 -0500

    include more data

--- DB/countydb.sql
+++ DB/countydb.sql
@@ -55,6 +55,14 @@ INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `
 INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (3, 'Allamakee', 1534, 89, 1426, 219, 39, 3307);
 INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (4, 'Appanoose', 923, 62, 956, 169, 20, 2130);
 INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (5, 'Audubon', 96, 8, 185, 20, 5, 314);
+INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (6, 'Benton', 417, 11, 453, 64, 6, 951);
+INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (7, 'Black Hawk', 369, 8, 340, 71, 5, 793);
+INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (8, 'Boone', 478, 31, 569, 96, 8, 1182);
+INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (9, 'Bremer', 458, 20, 372, 101, 7, 958);
+INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (10, 'Buchanan', 379, 16, 413, 83, 13, 904);
+INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (11, ' Woodbury', 306, 12, 434, 68, 4, 824);
+INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (12, 'Harrison', 469, 27, 555, 87, 9, 1147);
+INSERT INTO `county` (`id`, `county_name`, `doe`, `doe_fawn`, `antlered_buck`, `button_buck`, `shed_buck`, `total_deer`) VALUES (13, 'Monona', 431, 13, 440, 52, 9, 945);
 
 COMMIT;
 
